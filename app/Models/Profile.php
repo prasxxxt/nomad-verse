55ea60ad674d475a9aef87f12379e197cb31d572
Prashant <its.prashant.here@outlook.com>
2025-11-01 07:11:05 +0000

    added country model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    protected $fillable = ['user_id', 'role', 'bio', 'profile_photo', 'social_links'];
+    protected $fillable = [
+        'user_id', 'country_id', 'role', 'bio', 'profile_photo', 'social_links'
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
     }
 }
--- database/migrations/2025_11_01_064242_create_profiles_table.php
+++ database/migrations/2025_11_01_064242_create_profiles_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('role')->default('viewer');
+            $table->foreignId('country_id')->nullable()->references('id')->on('countries')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('role')->default('viewer'); // Role field added
             $table->text('bio')->nullable();
             $table->string('profile_photo')->nullable();
             $table->json('social_links')->nullable();
